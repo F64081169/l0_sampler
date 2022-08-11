6be75d2fd4ee3eb478d2fdaf0ec5ad9f9b9d0298
baobaomokk <baobaomokk@gmail.com>
2022-08-11 15:06:22 +0800

    ok

--- main.cpp
+++ main.cpp
@@ -2,15 +2,14 @@
 
 int main(){
     srand(time(NULL));
-    cout << "Please enter the number of n nodes, m edges, p passes and an interger k.\n";
-    cin >> vertexNum >> edges >> passes >> k;
+    cout << "Please enter the number of n nodes, m edges, and an interger k.\n";
+    cin >> vertexNum >> edges  >> k;
+    passes = 160*pow(k,2)*log10(vertexNum);
     for(int i = 0;i<vertexNum;i++){
         G.push_back(deque<int>{});
     }
-     
     int i = 0;
     fout.open("StreamingEdges.txt",ios::out); 
-    start_time = clock();
     while(i<edges){
         generate_edges(vertexNum);
         i++;
@@ -18,15 +17,19 @@ int main(){
     fout.close();
     fin.open("StreamingEdges.txt",ios::in);
     i = 0;
-    while(i<passes-1){
-        while(!fin.eof()){
+    start_time = clock();
+    while(i<passes){
+        // generate several random vertices in prob.(k/n)
+        while(!fin.eof()){ 
             fin >> sign >> x >> y;
-            stream_edges(sign,x,y);
+            // l0sampler to construct spanning trees
         }
         i++;
     }
-    fin.close();
+    //k-vertex connectivity algorithm
+    post_processing(k);
     finish_time = clock();
+    fin.close();
     // Final result
     cout << "-----Final result-----";
     print_graph(vertexNum,k);
@@ -35,40 +38,27 @@ int main(){
     return 0;
 }
 
+// void stream_edges(int sign,int x,int y){
+//     deque<int>::iterator itx = find_if(G[x].begin(), G[x].end(), bind2nd(equal_to<int>(), y));
+//     deque<int>::iterator ity = find_if(G[y].begin(), G[y].end(), bind2nd(equal_to<int>(), x));
+//     if(x!=y){
+//         if(sign==1){/* '+' */
+//             if((itx == G[x].end()) && (ity == G[y].end())){
+//                 G[x].push_back(y);
+//                 G[y].push_back(x);
+//             }
+//         }else{/* '-' */
+//             if((itx != G[x].end()) && (ity != G[y].end())){
+//                 G[x].erase(find(G[x].begin(),G[x].end(),y));
+//                 G[y].erase(find(G[y].begin(),G[y].end(),x));
+//             }
+//         }
+//     }
+// }
 
-void print_graph(int vertexNum,int k){
-    cout << "\nn = " << vertexNum << " ,m = " << edges << " ,p = " << passes << " ,k = " << k <<  endl;
-    for(int i = 0;i<vertexNum;i++){
-        cout << i << " : " ;
-        for(int j = 0;j<G[i].size();j++){
-            cout << G[i][j] << ' ';
-        }
-        cout << endl;
-    }
-    
-}
-void stream_edges(int sign,int x,int y){
-    deque<int>::iterator itx = find_if(G[x].begin(), G[x].end(), bind2nd(equal_to<int>(), y));
-    deque<int>::iterator ity = find_if(G[y].begin(), G[y].end(), bind2nd(equal_to<int>(), x));
-    if(x!=y){
-        if(sign==1){/* '+' */
-            if((itx == G[x].end()) && (ity == G[y].end())){
-                G[x].push_back(y);
-                G[y].push_back(x);
-            }
-        }else{/* '-' */
-            if((itx != G[x].end()) && (ity != G[y].end())){
-                G[x].erase(find(G[x].begin(),G[x].end(),y));
-                G[y].erase(find(G[y].begin(),G[y].end(),x));
-            }
-        }
-    }
-}
 void generate_edges(int vertexNum){
     x = rand()%vertexNum;
     y = rand()%vertexNum;
     int sign = rand()%2; 
-    fout << sign <<' '<< x << ' ' << y << endl;
-    stream_edges(sign,x,y);
-    
+    fout << sign <<' '<< x << ' ' << y << endl;   
 }
--- main.h
+++ main.h
@@ -7,6 +7,7 @@ using namespace std;
 #include <algorithm>
 #include <deque>
 #include <fstream>
+#include <math.h>
 
 /* 2D deque undirected graph G */
 deque<deque<int> > G;
@@ -16,4 +17,16 @@ clock_t start_time,finish_time;
 fstream fin,fout;
 void print_graph(int,int);
 void generate_edges(int);
-void stream_edges(int,int,int);
+// void stream_edges(int,int,int);
+void post_processing(int);
+
+void print_graph(int vertexNum,int k){
+    cout << "\nn = " << vertexNum << " ,m = " << edges << " ,p = " << passes << " ,k = " << k <<  endl;
+    for(int i = 0;i<vertexNum;i++){
+        cout << i << " : " ;
+        for(int j = 0;j<G[i].size();j++){
+            cout << G[i][j] << ' ';
+        }
+        cout << endl;
+    }   
+}
